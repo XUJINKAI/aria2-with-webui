85be147ef25aee0fccd4fdb6d40ae0b33cfa634b
XJK <XUJINKAI@users.noreply.github.com>
2018-11-26 11:48:42 +0800

    fix webui path

--- files/start.sh
+++ files/start.sh
@@ -12,6 +12,6 @@ fi
 chmod +x /conf/on-complete.sh
 touch /conf/aria2.session
 
-darkhttpd /aria2-webui --port 80 &
+darkhttpd /aria2-webui/docs --port 80 &
 darkhttpd /data --port 8080 &
 aria2c --conf-path=/conf/aria2.conf
